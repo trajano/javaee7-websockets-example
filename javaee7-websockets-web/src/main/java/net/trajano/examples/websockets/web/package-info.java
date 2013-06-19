ed10bf9624c639ba182512ec92c4786ba7f883d1
Archimedes Trajano <arch@trajano.net>
2013-06-19 17:21:10 -0400

    Added implementation of EchoSocket

---
+++ javaee7-websockets-web/src/main/java/net/trajano/examples/websockets/web/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Web tier classes.  Anything that would deal with web resources such as 
+ * Servlets, REST, Web Services, Web Sockets will go in this package.
+ */
+package net.trajano.examples.websockets.web;
